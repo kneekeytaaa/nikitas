90a593f18511c1e3bcc1e4ab313dde36549ab62c
Pulkit Singh <44489787+pulkitsingh69@users.noreply.github.com>
2021-01-03 13:00:50 +0530

    Update Brands.js

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export default class CtaButtons extends React.Component {
+export default class Brands extends React.Component {
     render() {
 
         <div className="brands">
