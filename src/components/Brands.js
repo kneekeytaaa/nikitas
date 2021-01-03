c8ab6a39253136362d2cc30dc3f0658555c8e9ae
Pulkit Singh <44489787+pulkitsingh69@users.noreply.github.com>
2021-01-03 13:00:07 +0530

    Create Brands.js

---
+++ src/components/Brands.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+
+export default class CtaButtons extends React.Component {
+    render() {
+
+        <div className="brands">
+        <div className="brand">
+          <img src="./assets/img/brands (1).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (14).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (2).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (3).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (4).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (5).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (6).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (7).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (8).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (9).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (15).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (11).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (12).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands (13).webp" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brand15.jpg" alt="none" />
+        </div>
+        <div className="brand">
+          <img src="./assets/img/brands16.jpg" alt="none" />
+        </div>
+
+        
+      </div>
+    }
+}
